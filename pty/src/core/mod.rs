70eb0d1e727e5e780eacd813da48ac9e6d4e2026
Cooksey99 <andrewcooksey42@gmail.com>
2025-11-27 19:42:47 -0600

    restructured and removed outdated code

--- pty/src/core/mod.rs
+++
@@ -1,9 +0,0 @@
-mod io_loop;
-mod ai_client;
-mod command;
-mod server_manager;
-mod input_handler;
-mod output_handler;
-
-pub use io_loop::*;
-pub use server_manager::*;
--- pty/src/utils/mod.rs
+++
@@ -1,5 +0,0 @@
-mod terminal;
-mod theme;
-
-pub use terminal::*;
-pub use theme::*;
